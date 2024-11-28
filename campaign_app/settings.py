34cc5d13ba9a5598a3d9f921c9bc12811bea8541
Sheshank Gupta <sheshankgupta@Mac.home>
2024-11-28 19:02:07 +0530

    allowed host

--- campaign_app/settings.py
+++ campaign_app/settings.py
@@ -116,11 +116,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['*']
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
